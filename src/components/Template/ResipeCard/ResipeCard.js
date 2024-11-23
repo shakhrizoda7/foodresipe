1a7732fe8818e49fb74738cc762fefc382cb630b
Your Name <shahrizodazayniddinova7@gmail.com>
2024-11-23 23:30:41 +0500

    food resipe

--- src/components/Template/ResipeCard/ResipeCard.js
+++ src/components/Template/ResipeCard/ResipeCard.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { Box, Card, CardContent, CardMedia, Collapse, Grid, IconButton, Typography } from "@mui/material";
+import { Box, Card, CardContent, CardMedia, CircularProgress, Collapse, Grid, IconButton, Typography } from "@mui/material";
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { ResipeCardStyled } from "./ResipeCardStyled";
 import { useSearch } from "../SearchContext";
@@ -9,6 +9,7 @@ const ResipeCard = () => {
   const { search } = useSearch();
   const [expanded, setExpanded] = useState({});
   const [foods, setFoods] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   // search foods
   const fetchRecipes = async () => {
@@ -17,6 +18,8 @@ const ResipeCard = () => {
       return;
     }
 
+    setLoading(true);
+
     try {
       const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`;
       const response = await axios.get(url);
@@ -24,6 +27,8 @@ const ResipeCard = () => {
     } catch (error) {
       console.error("Error fetching recipes:", error);
       setFoods([]);
+    }finally{
+      setLoading(false);
     }
   };
 
@@ -37,29 +42,36 @@ const ResipeCard = () => {
   return (
     <ResipeCardStyled>
       <Box className="food-cards">
-        <Grid container spacing={3}>
-          {foods.map((food) => (
-            <Grid item key={food.idMeal}  xs={12} sm={6} md={4}>
-              <Card className="cardBox">
-                <CardMedia component="img" height="194" image={food.strMealThumb} />
+          {loading ? (
+            <Box className='loadingBox'><CircularProgress/></Box>
+          ) : (
+            <Grid container spacing={3}>
+              {foods.length > 0 ? (
+                foods.map((food) => (
+                  <Grid item key={food.idMeal}  xs={12} sm={6} md={4}>
+                    <Card className="cardBox">
+                      <CardMedia component="img" height="194" image={food.strMealThumb} />
+  
+                      <CardContent sx={{maxHeight: 70, display: 'flex', alignItems: 'center'}}>
+                        <Typography variant="subtitle1" sx={{ color: '#16423C', fontWeight: '600' }}>{food.strMeal}</Typography>
 
-                <CardContent sx={{maxHeight: 70, display: 'flex', alignItems: 'center'}}>
-                  <Typography variant="h6" sx={{ color: '#16423C', fontWeight: '600' }}>{food.strMeal}</Typography>
-                  <IconButton onClick={() => toggleExpand(food.idMeal)} className={`expand-more ${expanded[food.idMeal] ? "expanded" : "collapsed"}`}>
-                    <ExpandMoreIcon />
-                  </IconButton>
-                </CardContent>
-
-                <Collapse in={expanded[food.idMeal]} timeout="auto" unmountOnExit>
-                  <CardContent>
-                    <Typography sx={{ marginBottom: 2 }}>Instructions:</Typography>
-                    <Typography sx={{ marginBottom: 2 }}>{food.strInstructions || "No instructions available"}</Typography>
-                  </CardContent>
-                </Collapse>
-              </Card>
+                        <IconButton onClick={() => toggleExpand(food.idMeal)} className={`expand-more ${expanded[food.idMeal] ? "expanded" : "collapsed"}`}>
+                          <ExpandMoreIcon />
+                        </IconButton>
+                      </CardContent>
+  
+                      <Collapse in={expanded[food.idMeal]} timeout="auto" unmountOnExit>
+                        <CardContent>
+                          <Typography sx={{ marginBottom: 2 }}>Instructions:</Typography>
+                          <Typography sx={{ marginBottom: 2 }}>{food.strInstructions || "No instructions available"}</Typography>
+                        </CardContent>
+                      </Collapse>
+                    </Card>
+                  </Grid>
+                ))
+              ) : (<Typography variant="h6" sx={{textAlign: 'center'}}>Not Found Resipe</Typography>)}
             </Grid>
-          ))}
-        </Grid>
+          )}
       </Box>
     </ResipeCardStyled>
   );
--- src/components/Template/ResipeCard/ResipeCardStyled.js
+++ src/components/Template/ResipeCard/ResipeCardStyled.js
@@ -15,30 +15,42 @@ export const ResipeCardStyled = styled.div`
   .food-cards{
     width: 100%;
     padding: 5% 12%;
-  }
-
-  .expand-more {
-    margin-left: auto;
-    transition: transform 0.15s ease-in-out; // Assuming the shortest duration
-
-    &.expanded {
-      transform: rotate(180deg);
-    }
-
-    &.collapsed {
-      transform: rotate(0deg);
-    }
-  }
-
-  .cardBox{
-    cursor: pointer;
-    color: #16423C;
-    max-width: 350px;
-    height: auto;
     display: flex;
+    align-items: center;
     flex-direction: column;
-    box-shadow: none;
-    background-color: #E9EFEC;
-    border-radius: 20px;
+    justify-content: center;
+
+    .loadingBox{
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      width: 100%;
+    }
+
+    .expand-more {
+      margin-left: auto;
+      transition: transform 0.15s ease-in-out; // Assuming the shortest duration
+  
+      &.expanded {
+        transform: rotate(180deg);
+      }
+  
+      &.collapsed {
+        transform: rotate(0deg);
+      }
+    }
+  
+    .cardBox{
+      cursor: pointer;
+      color: #16423C;
+      max-width: 350px;
+      height: auto;
+      display: flex;
+      flex-direction: column;
+      box-shadow: none;
+      background-color: #E9EFEC;
+      border-radius: 20px;
+    }
   }
+
 `;
